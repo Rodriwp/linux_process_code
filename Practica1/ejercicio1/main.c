c8dbe20c7afb05fce208384211891433b41ba0b8
GRADO EN INGENIERIA TELEMATICA <0330475@lab.it.uc3m.es>
2017-02-09 10:49:22 +0100

    Añadidos bucles. Falta añadir esperas.
     the commit message for your changes. Lines starting

--- Practica1/ejercicio1/main.c
+++ Practica1/ejercicio1/main.c
@@ -4,25 +4,42 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <time.h>
 
 void main(int argc, char *argv[])
 {
     pid_t pid = 0;
-    fprint("El padre esta ejecutando . . .");
     pid = fork();
     if (pid < 0){ /* error occurred */
-        fprintf(stderr, "Fork Failed");
-        exit(-l);
+        perror("Fork Failed");
+        exit(EXIT_FAILURE);
     }
-    else if (pid == 0) { /* child process */
-
+    else if (pid == 0) { /* child_1 process */
+	    while (1){
+	      printf("A, B, C");
+	      //sleep();
+	    }
     }
     else { /* parent process */
-
     /* parent waits for the child to complete */
-        wait(NULL);
-        printf("Child Complete");
-        exit(O);
+      pid = fork();
+      if (pid < 0){ /* error occurred */
+        perror("Fork Failed");
+        exit(EXIT_FAILURE);
+      }
+      else if (pid == 0) {/* child_2 process */
+	    while (1){
+	      printf("a, b, c");
+	      //sleep();
+	    }
+      }
+      while (1){
+	printf("El padre esta ejecutando . . .");
+	//sleep();
+      }
+      exit(EXIT_SUCCESS);
     }
 }
 
