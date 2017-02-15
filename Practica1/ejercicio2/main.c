2040719b04b6bc4ffb5c2eb484ea84280006a69a
rodrigo@rodrigomompo.com <rodrigo@rodrigomompo.com>
2017-02-15 15:29:45 +0100

    entregable final

--- Practica1/ejercicio2/main.c
+++ Practica1/ejercicio2/main.c
@@ -1,6 +1,8 @@
-//Practica 1: Ejercicio numero dos
-//@author Rodrigo Mompó Redoli
-//@author Jorge Serrano Dominguez
+/*Practica 1: Ejercicio 2
+ *
+ *@author Rodrigo Mompó Redoli
+ *@author Jorge Serrano Dominguez
+ */
 
 #include <stdio.h>
 #include <unistd.h>
@@ -26,20 +28,20 @@ void main(int argc, char *argv[])
         else if (pid == 0) { /* grandchild process */
             int ret2 = execlp("./fecha","fecha",NULL);
             if (ret2 == -1) {
-            perror("execl");
+            perror("execl: fecha");
             }
         }
         else { /* child process */
-            
+            wait(NULL);
             int ret = execl("/bin/ls", "ls", NULL);
             if (ret == -1) {
-            perror("execl");
+            perror("execl: ls");
             }
         }
     }
     else { /* parent process */
         wait(NULL);
-        wait(NULL);
+        printf("Mis hijos han terminado, adios\n");
         exit(EXIT_SUCCESS);
     }
 }
